7f0165420a6339c90ef23499d4866fdae56cd32f
Pietro Santos Miranda <161655720+pietros133@users.noreply.github.com>
2024-07-27 16:05:26 -0300

    Add files via upload

---
+++ ex0040.py
@@ -0,0 +1,18 @@
+#Declaração das Variaveis
+n1=float(input("Digite a Primeira Nota: "))
+n2=float(input("Digite a segunda Nota:"))
+# Calculo Necessario Para descobrir a média
+media=(n1+n2)/2
+# Verifica se o Resultado da Média é menor que Cinco
+if media < 5:
+    print("Estudar Mais, REPROAVADO")
+# Verifica se o resultado da média é maior que 5 e menor que Sete
+elif 7 > media >= 5:
+ print("ESTUDAR MAIS RECUPERAÇÃO")
+ # Verifica se o resultado da média é maior que Sete
+elif media >= 7:
+    print("Parabéns, Você foi APROVADO")
+else:
+    print("TU NEM É ALUNO")
+exit()
+
